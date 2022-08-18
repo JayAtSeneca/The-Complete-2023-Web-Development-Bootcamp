a861ee319d1bfe5566a79fcd752d9edd7026ed6a
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-08-18 01:31:10 -0400

    commit Thursday 08/18

--- toDoList-v1/app.js
+++ toDoList-v1/app.js
@@ -1,40 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.get("/", (req, res) => {
-  var today = new Date();
-  var currentDay = today.getDay();
-  let day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      day = "Invalid Day";
-  }
-  res.render("list", { kindOfDay: day });
+  let day = date.getDate();
+  res.render("list", { listTitle: day });
 });
+app.post('/',(req,res)=>{
+  let item = req.body.newItem;
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect('/work');
+  }
+  else{
+    items.push(item);
+    res.redirect('/');
+  }
+});
+
+app.get("/work",(req,res)=>{
+  res.render("list",{listTitle:"Work List",newListItems:workItems});
+})
+app.get('/about',(req,res)=>{
+  res.render('about');
+})
+
+app.post('/work',(req,res)=>{
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect('/work');
+})
 
 app.listen(3000, () => {
   console.log("Server Link: http://localhost:3000");
