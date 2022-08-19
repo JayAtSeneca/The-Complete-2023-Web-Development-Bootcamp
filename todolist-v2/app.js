31bc64295095ba4abe3cd55361c21214af8ac9f4
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-08-19 16:50:17 -0400

    remove functionality in to do list added

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -54,11 +54,23 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
   const itemName = req.body.newItem;
+  if(itemName){
   const item = new Item({
     name: itemName,
   });
   item.save();
+  }
   res.redirect("/");
+});
+
+app.post("/delete",(req,res)=>{
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId,(err)=>{
+    if(!err){
+      console.log("Successfully deleted checked item.");
+      res.redirect("/");
+    }
+  });
 });
 
 app.get("/work", function (req, res) {
