177f3cd43d4fe2ef4066775ca17a08bd3fe67984
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-08-19 19:00:01 -0400

    to do list complete

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -59,7 +60,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/:customListName", (req, res) => {
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({ name: customListName }, (err, foundedList) => {
     if (!err) {
@@ -117,12 +118,22 @@ app.post("/", function (req, res) {
 
 app.post("/delete", (req, res) => {
   const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+  if(listName === "Today") {
   Item.findByIdAndRemove(checkedItemId, (err) => {
     if (!err) {
       console.log("Successfully deleted checked item.");
       res.redirect("/");
     }
   });
+  }
+  else{
+    List.findOneAndUpdate({name: listName},{$pull: {items: {_id: checkedItemId}}},(err, foundedList) => {
+      if(!err){
+        res.redirect("/" + listName);
+      }
+    });
+  }
 });
 
 app.get("/work", function (req, res) {
