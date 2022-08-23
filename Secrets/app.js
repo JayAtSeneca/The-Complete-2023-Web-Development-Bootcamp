44645c04981d0c21ca4167c3f8f3b12c777668d2
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-08-23 17:40:48 -0400

    updated to level 4 authentication with bcrypt

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static('public'));
@@ -31,18 +32,23 @@ app.get("/login",(req, res)=>{
 
 app.post("/login", (req,res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, (err, foundUser)=>{
         if(err){
             console.log(err);
         }else{
-            if(foundUser.password === password){
-                res.render('secrets');
-            }
-            else{
-                res.send('Incorrect password');
-            }
+            if(foundUser){
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true){
+                        console.log(result);
+                        res.render('secrets');
+                    }
+                    else{
+                        res.send('Incorrect password');
+                    }
+                });
+            }          
         }
     });
 });
@@ -52,18 +58,21 @@ app.get("/register",(req, res)=>{
 });
 
 app.post("/register",(req, res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password),
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash,
+        });
+        newUser.save((err)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render('secrets');
+            }
+        });
     });
-    newUser.save((err)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render('secrets');
-        }
-    });
+   
 });
 
 app.use(function(req, res, next){
